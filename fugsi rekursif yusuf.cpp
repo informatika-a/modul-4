aaa2c0d8f71f9826451358c43d81bbfba445afa9
ikky <rizkisetiawan.root@gmail.com>
2016-12-19 13:18:36 +0700

    edited file from M.yusuf

--- fugsi rekursif yusuf.cpp
+++ fugsi rekursif yusuf.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <conio.h>
 using namespace std;
+int tampil=1;
 int fb(int n)
 {
-if (n==1)
-
-return 0;
-else if (n==2)
-return (1);
-else
-return (fb (n-1)+fb(n-2));
+    if(n>1){
+        cout<<tampil<<" ";
+        tampil++;fb(n-1);
+    }
+    else if(n<1){
+        cout<< 0;
+    }
+    else{
+        cout<<tampil;
+        tampil=1;
+    }
 }
 int main()
 {
-int n;
-cout<<"fungsi rekursif untuk mencari bilangan sampai ke-n : ";cin>>n;
-for(int i=1;i<=n;i++)
-cout<<fb(i)<<" ";
-
-cout<<endl;
+    int n;
+    cout<<"fungsi rekursif untuk mencari bilangan sampai ke-n : ";cin>>n;
+    fb(n);
+    cout<<endl;
 }
